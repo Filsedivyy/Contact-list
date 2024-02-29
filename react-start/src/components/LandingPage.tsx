d72805d2a0d76ffa6f2974f1a592143d7ff46e78
Filip Šedivý <sedivy@apparently.cz>
2024-02-29 13:27:31 +0100

    Tailwind- LandingPage

--- react-start/src/components/LandingPage.tsx
+++ react-start/src/components/LandingPage.tsx
@@ -1,11 +1,16 @@
-import "./style-LandingPage.css";
-
 const LandingPageComponent = ({ setActiveContactID }) => {
   return (
-    <div className="landing-page">
-      <h2>Žádné kontakty</h2>
-      <p>V listu momentálně nejsou žádné kontakty. Přidejte nějaký.</p>
-      <button onClick={() => setActiveContactID(-1)}>Přidat kontakt</button>
+    <div className="w-full px-[172px] pt-[48px]">
+      <h2 className="mt-[32px]">Žádné kontakty</h2>
+      <p className="mt-[16px]">
+        V listu momentálně nejsou žádné kontakty. Přidejte nějaký.
+      </p>
+      <button
+        className="mt-[32px] w-full h-[56px] bg-[#5DD661] rounded-[16px] text-white text-[16px] "
+        onClick={() => setActiveContactID(-1)}
+      >
+        Přidat kontakt
+      </button>
     </div>
   );
 };
