a5930503295894dc47369dbab93996b77cffa2c2
Filip Šedivý <sedivy@apparently.cz>
2024-02-29 14:08:07 +0100

    Tailwind- contactInfo

--- react-start/src/components/ContactInfo.tsx
+++ react-start/src/components/ContactInfo.tsx
@@ -1,23 +1,27 @@
-import "./style-ContactInfo.css";
 import InfoComponent from "./InfoComponent";
 
 const ContactInfo = ({ contact, deleteContact, editContact }) => {
   return (
-    <div className="contact-info">
-      <header>
-        <div className="placeholder"></div>
+    <div className="flex flex-col items-center w-full">
+      <header className="w-full flex items-center justify-center h-[48px] border-b border-[#E3E3E3] py-[4px]">
         <h2>{contact.fullName}</h2>
-        <div className="button-container">
-          <button onClick={() => deleteContact(contact.id)}>
+        <div className="flex gap-[8px] absolute right-[8px] top-[4px]">
+          <button
+            className="size-[40px] p-[8px] flex items-center justify-center"
+            onClick={() => deleteContact(contact.id)}
+          >
             <img src="./delete.svg" alt="" />
           </button>
-          <button onClick={() => editContact(contact.id)}>
+          <button
+            className="size-[40px] p-[8px] flex items-center justify-center"
+            onClick={() => editContact(contact.id)}
+          >
             <img src="./edit.svg" alt="" />
           </button>
         </div>
       </header>
-      <main>
-        <h1>{contact.fullName}</h1>
+      <main className="w-full px-[172px]">
+        <h1 className="mt-[32px] mb-[16px]">{contact.fullName}</h1>
         <InfoComponent img={"email.svg"} type={"Email"} value={contact.email} />
         <InfoComponent
           img={"phone.svg"}
--- react-start/src/components/InfoComponent.tsx
+++ react-start/src/components/InfoComponent.tsx
@@ -1,16 +1,14 @@
-import "./style-InfoComponent.css";
-
 const InfoComponent = ({ img, type, value }) => {
   return (
-    <div className="info-component">
-      <div className="content">
+    <div className="w-full border-b border-solid border-[#E3E3E3] flex items-center justify-between py-[14px] pl-[16px]">
+      <div className="flex gap-[14px] items-center">
         <img src={img} alt="" />
-        <div className="container">
-          <h3>{type}</h3>
-          <p>{value}</p>
+        <div className="flex flex-col gap-[2px] h-fit">
+          <h3 className="text-[14px]">{type}</h3>
+          <p className="text-[14px] text-[#545454]">{value}</p>
         </div>
       </div>
-      <button>
+      <button className="h-[40px] w-[40px]">
         <img src="copy.svg" alt="" />
       </button>
     </div>
