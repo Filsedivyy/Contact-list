0347a1863a849e7bcc107c762e6e97781789328b
Filip Šedivý <sedivy@apparently.cz>
2024-04-12 14:22:41 +0200

    -

--- react-start/src/app.tsx
+++ react-start/src/app.tsx
@@ -98,6 +98,7 @@ const App = () => {
             onClick={() => {
               cancelFunc();
               setInputValue("");
+              setSorted(false);
             }}
           >
             Kontakty
